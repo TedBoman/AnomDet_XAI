7c75c22f36dd15db7a23d451462b886833c6381a
Liamburberry <lbm22005@student.mdu.se>
2024-12-09 15:01:29 +0100

    upd

--- Backend/execute_calls.py
+++ Backend/execute_calls.py
@@ -1,5 +1,6 @@
 from Backend.ML_models.lstm import LSTMModel
 from Backend.ML_models.isolation_forest import IsolationForest
+from Database.db_interface import DBInterface
 import pandas as pd
 import os
 
@@ -8,17 +9,17 @@ def run_batch(model: str, injection_method: str, file_path: str) -> str:
     dataset_folder = "Datasets"
     full_path = os.path.join(dataset_folder, file_path)
     df = pd.read_csv(full_path)
-    
+
     match model:
         case "lstm":
             lstm_instance = LSTMModel()
             detection_df = lstm_instance.run(df)
             anomalies = lstm_instance.detect(detection_df)
             pass
+        
         case "isolation forest":
             if_instance = IsolationForest()
             detection_df = if_instance.run(df)
             anomalies = if_instance.detect(detection_df)
             pass
 
-    
