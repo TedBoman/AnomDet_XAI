1d8b542ae8d22b0084b2325c2d14b89483629c98
Liamburberry <lbm22005@student.mdu.se>
2024-12-09 12:01:03 +0100

    upd

--- Backend/execute_calls.py
+++ Backend/execute_calls.py
@@ -1,2 +1,13 @@
+
+
 def run_batch(model: str, injection_method: str, file_path: str) -> str:
+
+    match model:
+        case "lstm":
+
+            pass
+        case "isolation forest":
+
+            pass
+
     
