e2ec95a7f855d17edf100ddbfe98eea07db88809
Liamburberry <lbm22005@student.mdu.se>
2024-12-17 13:47:18 +0100

    Adding Callbacks
    callbacks to store: model, dataset & injection method

--- Frontend/pages/action_handler.py
+++ Frontend/pages/action_handler.py
@@ -1,6 +1,14 @@
 from api import BckendAPI
 
 
-def user_actions():
+def user_request(selected_dataset, selected_model, selected_inj_method):
     
-    pass
+    match selected_inj_method:
+        case None:
+            BckendAPI.get_dataset(selected_dataset)
+            BckendAPI.get_model(selected_model)
+        case _:
+            BckendAPI.get_dataset(selected_dataset)
+            BckendAPI.get_model(selected_model)
+            BckendAPI.get_injection_method(selected_inj_method)
+    
