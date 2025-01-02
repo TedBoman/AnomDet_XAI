ce85b214aca6d7b0beb4db70eb4f09ca16c27c7e
MarcusHammarstrom <marham1995@gmail.com>
2025-01-02 22:23:25 +0100

    added error handling in frontend_handler

--- Frontend/pages/frontend_handler.py
+++ Frontend/pages/frontend_handler.py
@@ -6,22 +6,28 @@ from api import BackendAPI
 class FrontendHandler:
     def __init(self, host, port):
         self.api = BackendAPI(host, port)
+
+    def __check_name(self, job_name) -> str:
+        response = self.handle_get_all_jobs()
+        if job_name in response["jobs"]:
+            return "name-error"
+        return "success"
     
     def handle_run_batch(self, selected_dataset, selected_model, job_name, inj_params: dict=None) -> str:
-        response = self.handle_get_all_jobs()
-        if job_name in response["jobs"]:
-            return "name-error"
+        response = self.__check_name(job_name)
 
-        self.api.run_batch(selected_model, selected_dataset, job_name, inj_params=inj_params)
-        return "success"
+        if response == "success":
+           self.api.run_batch(selected_model, selected_dataset, job_name, inj_params=inj_params)
+
+        return response
 
     def handle_run_stream(self, selected_dataset, selected_model, job_name, inj_params: dict=None) -> str:
-        response = self.handle_get_all_jobs()
-        if job_name in response["jobs"]:
-            return "name-error"
+        response = self.__check_name(job_name)
 
-        self.api.run_stream(selected_model, selected_dataset, job_name, inj_params=inj_params)
-        return "success"
+        if response == "success":
+           self.api.run_stream(selected_model, selected_dataset, job_name, inj_params=inj_params)
+
+        return response
 
     def handle_change_model(self, selected_model, job_name):
         return self.api.change_model(selected_model, job_name)
@@ -36,7 +42,12 @@ class FrontendHandler:
         return self.api.get_running()
 
     def handle_cancel_job(self, job_name):
-        self.api.cancel_job(job_name)
+        response = self.__check_name(job_name)
+
+        if response == "success":
+           self.api.cancel_job(job_name)
+
+        return response
 
     def handle_get_models(self):
         return self.api.get_models()
@@ -47,11 +58,16 @@ class FrontendHandler:
     def handle_get_datasets(self):
         return self.api.get_datasets()
 
-    def handle_import_dataset(self, file_path, timestamp_column=None):
+    def handle_import_dataset(self, file_path, timestamp_column: str):
         self.api.import_dataset(file_path, timestamp_column)
 
     def handle_get_all_jobs(self):
         self.api.get_all_jobs()
 
     def handle_get_columns(self, job_name):
-        return self.api.get_columns(job_name)
+        response = self.__check_name(job_name)
+
+        if self.__check_name(job_name) == "success":
+           return self.api.get_columns(job_name)
+
+        return response
